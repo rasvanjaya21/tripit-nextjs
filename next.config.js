afc5822e533d646f1bc1b4268829746db8fb7849
rasvanjaya21 <mrsejati17@gmail.com>
2023-11-17 22:40:02 +0700

    permit img.shields.io

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		dangerouslyAllowSVG: true,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.shields.io",
+			},
+		],
+	},
+};
 
 module.exports = nextConfig;
