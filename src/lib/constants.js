1da76118ab6cc076b4e02a914788b39bc180d3a5
rasvanjaya21 <mrsejati17@gmail.com>
2023-11-17 17:00:24 +0700

    added constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,46 @@
+const sidebarMenu = [
+	{
+		id: 1,
+		name: "Home",
+		route: "/admin",
+		icon: "/assets/home-icon.svg",
+		current: true,
+	},
+	{
+		id: 2,
+		name: "All trips",
+		route: "/admin/all-trips",
+		icon: "/assets/all-trips-icon.svg",
+		current: false,
+	},
+	{
+		id: 3,
+		name: "Travels",
+		route: "/admin/travels",
+		icon: "/assets/travels-icon.svg",
+		current: false,
+	},
+	{
+		id: 4,
+		name: "Rooms",
+		route: "/admin/rooms",
+		icon: "/assets/rooms-icon.svg",
+		current: false,
+	},
+	{
+		id: 5,
+		name: "Transport",
+		route: "/admin/transport",
+		icon: "/assets/transport-icon.svg",
+		current: false,
+	},
+	{
+		id: 6,
+		name: "Attractions",
+		route: "/admin/attractions",
+		icon: "/assets/attractions-icon.svg",
+		current: false,
+	},
+];
+
+export { sidebarMenu };
